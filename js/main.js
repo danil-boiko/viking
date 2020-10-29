0c1aa1e5e020155162a92df96782b8cca4036e7a
danil-boiko <boiko.81@list.ru>
2020-10-29 23:38:27 +0300

    moblie version for heroes-section and footer

--- js/main.js
+++ js/main.js
@@ -5,7 +5,15 @@ $(function () {
         slidesToScroll: 1,
         asNavFor: '.heroes__slider-text',
         prevArrow: '  <button class="slick-btn slick-prev"><img src="images/arrow-left.png" alt="prev"></button>',
-        nextArrow: '  <button class="slick-btn slick-next"><img src="images/arrow-right.png" alt="next"></button>'
+        nextArrow: '  <button class="slick-btn slick-next"><img src="images/arrow-right.png" alt="next"></button>',
+        responsive: [
+            {
+                breakpoint: 769,
+                settings: {
+                    arrows: false,
+                }
+            },
+        ]
     });
 
     $('.heroes__slider-text').slick({
